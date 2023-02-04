29b9829baf7902ea9cd20fa52f8db3ba9d318161
Sander92000 <groot.sander@hotmail.fr>
2023-02-04 16:35:58 +0100

    GO

--- src/App.vue
+++ src/App.vue
@@ -1,123 +1,50 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
+  <q-layout view="lHh Lpr lFf">
+    <q-header id="page-header" elevated class="glossy">
       <h1>Rafinato Equine Dentistery</h1>
-      <span id="subtitle">Linda Groot – Technicienne Dentaire Equin</span>
-    </div>
-  </header>
+      <p>Linda Groot – Technicienne Dentaire Equin</p>
+      <q-btn color="primary" label="Demande rendez-vous" />
+    </q-header>
 
-  <div class="router-view-container">
-    <RouterView />
-  </div>
+    <q-page-container id="page-container">
+      <router-view></router-view>
+    </q-page-container>
 
-  <footer>
-    <div class="container">
-      <div class="row signature">
-        <p>Rafinato Equine Dentistery</p>
-      </div>
-      <div class="row social">
-        <p>Retrouvez nous sur:</p>
-          <ul>
-            <li v-for="link in socialLinks" v-bind:key="link.id">
-              <a :href="link.link" :target="link.target">
-                <img :src="link.logo" :alt="link.alt" />
-              </a>
-            </li>
-          </ul>
-      </div>
-    </div>
-  </footer>
+    <q-footer id="page-footer">
+      <p>Sander</p>
+    </q-footer>
+  </q-layout>
 </template>
 
-<style scoped>
-header{
-  width: 100%;
-  margin-top: 32px;
-  margin-bottom: 32px;
-}
+<script>
+import { ref } from 'vue'
 
-.wrapper{
-  width: 80%;
-  margin: auto;
-}
-
-h1 {
-  font-style: bold;
-  margin-bottom: 0px;
-}
-
-#subtitle {
-  margin-top: 0px;
-  color: #757575;
-  font-size: 0.75614rem;
-  font-family: "PT Sans", -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif;
-  font-weight: 400;
-}
-
-.router-view-container {
-  min-height: 70vh;
-}
-
-footer {
-  width: 100%;
-}
-
-.signature {
-  padding: 15px;
-  align-items: center;
-  text-align: center;
-  font-weight: bold;
-}
-
-.social p {
-  font-size: 16px;
-}
-
-.social ul{
-  display: flex;
-  justify-items: center;
-  align-items: center;
-  justify-content: center;
-}
-
-.social ul li {
-  display: inline;
-  list-style: none;
-  margin: 10px;
-}
-
-.social ul li img {
-  width: 32px;
-}
-
-
-#fb-logo {
-  width: 58px;
-}
-
-#fb-logo img{
-  width: 32px;
-}
-</style>
-
-<script lang="ts">
 export default {
-  data: () => {
-    return {
-      socialLinks: [
-        {
-          id: 1,
-          link: "https://www.facebook.com/linda.groot.944",
-          target: "_blank",
-          logo: "./f_logo_58.png",
-          alt: "Logo facebook",
-        },
-      ],
-    };
+  name: 'LayoutDefault',
+
+  components: {
   },
+
+  setup () {
+    return {
+    }
+  }
 }
 </script>
+
+<style lang="scss" scoped>
+#page-header{
+  padding: 15px;
+  background-color: #fff;
+}
+
+#page-container {
+  padding-top: 0px !important;
+  padding-bottom: 0px !important;
+}
+
+#page-footer {
+  padding: 15px;
+  background-color: #fff;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import App from './App.vue'
 import router from './router'
 
 import './assets/main.css'
+import { Quasar } from 'quasar'
+import quasarUserOptions from './quasar-user-options'
 
-const app = createApp(App)
+const app = createApp(App).use(Quasar, quasarUserOptions);
 
-app.use(createPinia())
-app.use(router)
+app.use(createPinia());
+app.use(router);
 
-app.mount('#app')
+app.mount("#app");
